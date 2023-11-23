ecd1f0ca9fd959023ad56c4bd189baa1e5e34da9
Hồ Anh Tín <84175222+hoanhtin1703@users.noreply.github.com>
2023-11-24 01:21:10 +0700

    Update Login, Post,Profile

--- src/components/dashboard/components/Main.component.js
+++ src/components/dashboard/components/Main.component.js
@@ -1,148 +1,151 @@
-import React, {useEffect, useState} from "react";
-import {toast} from "react-toastify";
-import {Modal, Post, LoadingPost, LoadingForm, FormCreatePost} from "../..";
+import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { Modal, Post, LoadingPost, LoadingForm, FormCreatePost } from "../..";
 import InfiniteScroll from "react-infinite-scroll-component";
 
 const Center = ({
-    posts,
-    loading,
-    token,
-    autoFetch,
-    setOneState,
-    dark,
-    user,
-    getAllPosts,
-    setPosts,
-    getNewPosts,
-    error,
-    isQrCode,
+  posts,
+  loading,
+  token,
+  autoFetch,
+  setOneState,
+  dark,
+  user,
+  getAllPosts,
+  setPosts,
+  getNewPosts,
+  error,
+  isQrCode,
 }) => {
-    const [attachment, setAttachment] = useState("");
-    const [text, setText] = useState("");
-    const [openModal, setOpenModal] = useState(false);
-    const [loadingCreateNewPost, setLoadingCreateNewPost] = useState(false);
+  const [attachment, setAttachment] = useState("");
+  const [text, setText] = useState("");
+  const [openModal, setOpenModal] = useState(false);
+  const [loadingCreateNewPost, setLoadingCreateNewPost] = useState(false);
 
-    // Modal
-    useEffect(() => {
-        setOneState("openModal", openModal);
-    }, [openModal]);
+  // Modal
+  useEffect(() => {
+    setOneState("openModal", openModal);
+  }, [openModal]);
 
-    // get posts
-    useEffect(() => {
-        if (token) {
-            getAllPosts();
+  // get posts
+  useEffect(() => {
+    if (token) {
+      getAllPosts();
+    }
+  }, [token]);
+
+  const createNewPost = async (formData) => {
+    setLoadingCreateNewPost(true);
+    if (!text) {
+      toast.error("You must type something...");
+      return;
+    }
+    try {
+      if (formData) {
+        console.log(formData.getAll("file"));
+        const { data } = await autoFetch.post(`createnewpost`, formData);
+        if (data.status === true) {
+          setPosts([data.post, ...posts]);
+          toast.success(data.message);
+          setLoadingCreateNewPost(false);
+        } else {
+          toast.error(data.message);
+          return;
         }
-    }, [token]);
-
-    const createNewPost = async (formData) => {
-        setLoadingCreateNewPost(true);
-        if (!text) {
-            toast.error("You must type something...");
-            return;
-        }
-        try {
-            let image = null;
-            if (formData) {
-                const {data} = await autoFetch.post(
-                    `/api/post/upload-image`,
-                    formData
-                );
-                image = {url: data.url, public_id: data.public_id};
-            }
-
-            const {data} = await autoFetch.post(`api/post/create-post`, {
-                content: text,
-                image,
-            });
-            setPosts([data.post, ...posts]);
-        } catch (error) {
-            console.log(error);
-        }
+      } else {
+        toast.error("Some thing went wrong");
         setLoadingCreateNewPost(false);
-    };
+      }
+    } catch (error) {
+      toast.error(new Error() || "Some thing went wrong");
+      console.log(error);
+    }
+  };
 
-    const content = () => {
-        if (loading) {
-            return (
-                <div>
-                    <LoadingPost />
-                </div>
-            );
-        }
-        if (error) {
-            return (
-                <div
-                    className={`bg-white ${
-                        !dark && "shadow-post"
-                    } dark:bg-[#242526] rounded-lg w-full text-center text-xl font-bold py-10 `}>
-                    <div>No post found... Try again!</div>
-                </div>
-            );
-        }
-        if (posts.length === 0) {
-            return (
-                <div className='w-full text-center text-xl font-semibold pt-[20vh] flex-col '>
-                    <div>
-                        You don't post anything and don't follow anyone.
-                        <br />
-                        Let's do something! :3
-                    </div>
-                </div>
-            );
-        }
-        return (
-            <InfiniteScroll
-                dataLength={posts.length}
-                next={getNewPosts}
-                hasMore={true}
-                loader={<LoadingPost />}>
-                {posts.map((post) => (
-                    <Post
-                        key={post._id}
-                        currentPost={post}
-                        user_img={user.image.url}
-                        userId={user._id}
-                        className={!dark ? "shadow-post" : ""}
-                        userRole={user.role}
-                    />
-                ))}
-            </InfiniteScroll>
-        );
-    };
-
-    const form = () => {
-        if (error) {
-            return <></>;
-        }
-        if (loading) return <LoadingForm />;
-        return (
-            <FormCreatePost
-                setAttachment={setAttachment}
-                setOpenModal={setOpenModal}
-                text={text}
-                user={user}
-            />
-        );
-    };
-
-    return (
-        <div className=''>
-            {form()}
-
-            {openModal && !isQrCode && (
-                <Modal
-                    setOpenModal={setOpenModal}
-                    text={text}
-                    setText={setText}
-                    attachment={attachment}
-                    setAttachment={setAttachment}
-                    createNewPost={createNewPost}
-                />
-            )}
-            {loadingCreateNewPost && <LoadingPost className='mb-4' />}
-            {content()}
+  const content = () => {
+    if (loading) {
+      return (
+        <div>
+          <LoadingPost />
         </div>
+      );
+    }
+    if (error) {
+      return (
+        <div
+          className={`bg-white ${
+            !dark && "shadow-post"
+          } dark:bg-[#242526] rounded-lg w-full text-center text-xl font-bold py-10 `}
+        >
+          <div>No post found... Try again!</div>
+        </div>
+      );
+    }
+    if (posts.length === 0) {
+      return (
+        <div className="w-full text-center text-xl font-semibold pt-[20vh] flex-col ">
+          <div>
+            You don't post anything and don't follow anyone.
+            <br />
+            Let's do something! :3
+          </div>
+        </div>
+      );
+    }
+    return (
+      <InfiniteScroll
+        dataLength={posts.length}
+        next={getNewPosts}
+        hasMore={true}
+        loader={<LoadingPost />}
+      >
+        {posts.map((post) => (
+          <Post
+            key={post._id}
+            currentPost={post}
+            user_img={user.profilePicture}
+            userId={post.author._id}
+            className={!dark ? "shadow-post" : ""}
+            userRole={user.isAdmin}
+          />
+        ))}
+      </InfiniteScroll>
     );
+  };
+
+  const form = () => {
+    if (error) {
+      return <></>;
+    }
+    if (loading) return <LoadingForm />;
+    return (
+      <FormCreatePost
+        setAttachment={setAttachment}
+        setOpenModal={setOpenModal}
+        text={text}
+        user={user}
+      />
+    );
+  };
+
+  return (
+    <div className="">
+      {form()}
+
+      {openModal && !isQrCode && (
+        <Modal
+          setOpenModal={setOpenModal}
+          text={text}
+          setText={setText}
+          attachment={attachment}
+          setAttachment={setAttachment}
+          createNewPost={createNewPost}
+        />
+      )}
+      {loadingCreateNewPost && <LoadingPost className="mb-4" />}
+      {content()}
+    </div>
+  );
 };
 
 export default Center;
--- src/context/useContext.js
+++ src/context/useContext.js
@@ -1,129 +1,128 @@
-import {createContext, useContext, useState} from "react";
+import { createContext, useContext, useState } from "react";
 import axios from "axios";
-import {toast} from "react-toastify";
+import { toast } from "react-toastify";
 
 const user = JSON.parse(localStorage.getItem("user"));
 const token = JSON.parse(localStorage.getItem("token"));
 const dark = JSON.parse(localStorage.getItem("dark"));
 const initState = {
-    user: user || "",
-    token: token || "",
-    dark: dark || false,
-    openModal: false,
-    isQrCode: false,
+  user: user || "",
+  token: token || "",
+  dark: dark || false,
+  openModal: false,
+  isQrCode: false,
 };
 // @ts-ignore
 const AppContext = createContext();
 
-const AppProvider = ({children}) => {
-    const [state, setStateContext] = useState(initState);
+const AppProvider = ({ children }) => {
+  const [state, setStateContext] = useState(initState);
 
-    if (state.dark) {
-        document.documentElement.classList.add("dark");
-    } else {
-        document.documentElement.classList.remove("dark");
+  if (state.dark) {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+  if (state.openModal) {
+    document.body.classList.add("modal-open");
+  } else {
+    document.body.classList.toggle("modal-open", false);
+    document.body.classList.add("style-3");
+  }
+  axios.defaults.headers.common["Authorization"] = `Bearer ${state.token}`;
+
+  const autoFetch = axios.create({
+    // @ts-ignore
+    baseURL: "http://localhost:7000/",
+  });
+
+  // Add a request interceptor
+  autoFetch.interceptors.request.use(
+    function (config) {
+      // Do something before request is sent
+      return config;
+    },
+    function (error) {
+      // Do something with request error
+      return Promise.reject(error);
     }
-    if (state.openModal) {
-        document.body.classList.add("modal-open");
-    } else {
-        document.body.classList.toggle("modal-open", false);
-        document.body.classList.add("style-3");
+  );
+  // Add a response interceptor
+  autoFetch.interceptors.response.use(
+    function (response) {
+      // Any status code that lie within the range of 2xx cause this function to trigger
+      // Do something with response data
+      return response;
+    },
+    function (error) {
+      // Any status codes that falls outside the range of 2xx cause this function to trigger
+      // Do something with response error
+      if (error.response.status === 401) {
+        toast.error("Your session has expired. Please log in again.");
+        logOut();
+      }
+      if (error.response.status === 403) {
+        toast.error("This page is for admin use only. Please log in again.");
+        logOut();
+      }
+      if (error.response.status === 11000) {
+        toast.error("Something went wrong. Try again!");
+      }
+      return Promise.reject(error);
     }
-    axios.defaults.headers.common["Authorization"] = `Bearer ${state.token}`;
+  );
+  const removeFromLocalStorage = () => {
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+  };
 
-    const autoFetch = axios.create({
-        // @ts-ignore
-        baseURL: process.env.REACT_APP_SERVER_URL,
-    });
+  const addToLocalStorage = (user, token) => {
+    localStorage.setItem("user", JSON.stringify(user));
+    localStorage.setItem("token", JSON.stringify(token));
+  };
 
-    // Add a request interceptor
-    autoFetch.interceptors.request.use(
-        function (config) {
-            // Do something before request is sent
-            return config;
-        },
-        function (error) {
-            // Do something with request error
-            return Promise.reject(error);
-        }
-    );
-    // Add a response interceptor
-    autoFetch.interceptors.response.use(
-        function (response) {
-            // Any status code that lie within the range of 2xx cause this function to trigger
-            // Do something with response data
-            return response;
-        },
-        function (error) {
-            // Any status codes that falls outside the range of 2xx cause this function to trigger
-            // Do something with response error
-            if (error.response.status === 401) {
-                toast.error("Your session has expired. Please log in again.");
-                logOut();
-            }
-            if (error.response.status === 403) {
-                toast.error(
-                    "This page is for admin use only. Please log in again."
-                );
-                logOut();
-            }
-            if (error.response.status === 11000) {
-                toast.error("Something went wrong. Try again!");
-            }
-            return Promise.reject(error);
-        }
-    );
-    const removeFromLocalStorage = () => {
-        localStorage.removeItem("user");
-        localStorage.removeItem("token");
-    };
+  const setOneState = (name, value) => {
+    setStateContext({ ...state, [name]: value });
+  };
 
-    const addToLocalStorage = (user, token) => {
-        localStorage.setItem("user", JSON.stringify(user));
-        localStorage.setItem("token", JSON.stringify(token));
-    };
+  const setNameAndToken = (userValue, tokenValue) => {
+    setStateContext({ ...state, user: userValue, token: tokenValue });
+    addToLocalStorage(userValue, tokenValue);
+  };
 
-    const setOneState = (name, value) => {
-        setStateContext({...state, [name]: value});
-    };
+  const logOut = () => {
+    removeFromLocalStorage();
+    setStateContext({ ...state, user: "", token: "" });
+  };
 
-    const setNameAndToken = (userValue, tokenValue) => {
-        setStateContext({...state, user: userValue, token: tokenValue});
-        addToLocalStorage(userValue, tokenValue);
-    };
+  const openQrCode = () => {
+    setStateContext({ ...state, openModal: true, isQrCode: true });
+  };
+  const closeQrCode = () => {
+    setStateContext({ ...state, openModal: false, isQrCode: false });
+  };
 
-    const logOut = () => {
-        removeFromLocalStorage();
-        setStateContext({...state, user: "", token: ""});
-    };
-
-    const openQrCode = () => {
-        setStateContext({...state, openModal: true, isQrCode: true});
-    };
-    const closeQrCode = () => {
-        setStateContext({...state, openModal: false, isQrCode: false});
-    };
-
-    return (
-        <AppContext.Provider
-            value={{
-                ...state,
-                autoFetch,
-                setStateContext,
-                setOneState,
-                logOut,
-                setNameAndToken,
-                addToLocalStorage,
-                openQrCode,
-                closeQrCode,
-            }}>
-            {children}
-        </AppContext.Provider>
-    );
+  return (
+    <AppContext.Provider
+      value={{
+        ...state,
+        autoFetch,
+        setStateContext,
+        setOneState,
+        logOut,
+        setNameAndToken,
+        addToLocalStorage,
+        openQrCode,
+        closeQrCode,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 const useAppContext = () => {
-    return useContext(AppContext);
+  return useContext(AppContext);
 };
 
-export {useAppContext, AppProvider};
+export { useAppContext, AppProvider };
